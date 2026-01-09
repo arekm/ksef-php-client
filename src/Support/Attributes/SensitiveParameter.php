1607da8dbf74e7fd5c6fe0298b58aaf6cf89530a
N1ebieski <kontakt@intelekt.net.pl>
2026-01-09 19:03:56 +0100

    add final

--- src/Support/Attributes/SensitiveParameter.php
+++ src/Support/Attributes/SensitiveParameter.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 if (PHP_VERSION_ID < 80200 && ! class_exists('SensitiveParameter')) {
     #[Attribute(Attribute::TARGET_PARAMETER)]
-    class SensitiveParameter
+    final class SensitiveParameter
     {
     }
 }
